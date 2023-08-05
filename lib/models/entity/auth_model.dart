f47362b72e2d2bba677884ce191ed48c682b8d62
Emre Gültekir <gltkr.emre@gmail.com>
2023-08-05 15:53:53 +0300

    request and response models created.

---
+++ lib/models/entity/auth_model.dart
@@ -0,0 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../core/services/models/entity_model.dart';
+import '../../core/services/models/token_model.dart';
+
+part 'auth_model.g.dart';
+
+@JsonSerializable(createToJson: false)
+class AuthModel extends TokenModel implements EntityModel<TokenModel> {
+  AuthModel({super.expirationDate, super.refreshExpirationDate, super.refreshToken, super.token});
+  @override
+  AuthModel fromJson(Map<String, dynamic> json) => _$AuthModelFromJson(json);
+}
---
+++ lib/models/entity/auth_model.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AuthModel _$AuthModelFromJson(Map<String, dynamic> json) => AuthModel(
+      expirationDate: json['expirationDate'] as String?,
+      refreshExpirationDate: json['refreshExpirationDate'] as String?,
+      refreshToken: json['refreshToken'] as String?,
+      token: json['token'] as String?,
+    );
