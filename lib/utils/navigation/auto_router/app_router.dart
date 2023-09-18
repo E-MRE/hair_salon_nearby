618713938a55ac33c2117a7ff81d11b6ecfa29ac
Emre Gültekir <gltkr.emre@gmail.com>
2023-09-18 23:37:42 +0300

    auth guard created

--- lib/utils/navigation/auto_router/app_router.dart
+++ lib/utils/navigation/auto_router/app_router.dart
@@ -8,6 +8,7 @@ import '../../../presentation/screens/onboard/view/onboard_page.dart';
 import '../../../presentation/screens/splash/view/splash_page.dart';
 import '../../constants/router_name_constants.dart';
 import '../../enum/auth_type.dart';
+import 'helpers/auto_route_by_auth_guard.dart';
 
 part 'app_router.gr.dart';
 
@@ -15,10 +16,14 @@ part 'app_router.gr.dart';
 @AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends _$AppRouter {
   List<AutoRoute> get routes => <AutoRoute>[
-        AutoRoute(page: SplashRoute.page, initial: true, path: RouterPathConstants.splash),
+        AutoRoute(
+          page: SplashRoute.page,
+          initial: true,
+          path: RouterPathConstants.splash,
+        ),
         AutoRoute(page: OnboardRoute.page, path: RouterPathConstants.onboard),
         AutoRoute(page: LoginRoute.page, path: RouterPathConstants.login),
         AutoRoute(page: RegisterRoute.page, path: RouterPathConstants.register),
-        AutoRoute(page: MenuRoute.page, path: RouterPathConstants.menu),
+        AutoRouteByAuthGuard(page: MenuRoute.page, path: RouterPathConstants.menu),
       ];
 }
